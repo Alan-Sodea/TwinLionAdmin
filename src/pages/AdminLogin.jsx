12a09ef80533ac34d0d6fe354c7970cf246f4f4f
KONZOU SODEA Alan Pérec <konzousodea@gmail.com>
2024-12-27 21:38:36 +0100

    Update AdminLogin.jsx

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -21,7 +21,7 @@ export const AdminLogin = () => {
             alt="Twin Lions"
           />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-primary">
-            Administration
+            Administration Twin Lions
           </h2>
         </div>
 
